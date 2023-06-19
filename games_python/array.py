b40fe135e35a4de658f606b87e30ae2901fc29fb
luca <lucagiustini92@gmail.com>
2023-06-19 09:38:01 -0700

    adding scritps

--- games_python/array.py
+++ games_python/array.py
@@ -1,4 +1,44 @@
+# import numpy library
 import numpy as np
 
-my_array = np.array([1, 2, 3, 4, 5])
-print(my_array)
+# create an array with numpy library
+array = np.array([1, 2, 3, 4, 5])
+# print the first element of the array
+print(array[0])
+
+# create a string array with numpy library
+array = np.array(["a", "b", "c", "d", "e"])
+# print the second element of the array
+print(array[1])
+
+# create a 2D array with numpy library
+array = np.array([[1, 2, 3], [4, 5, 6]])
+# print the first raw of the array
+print(array[0])
+# print the second element of the first raw of the array
+print(array[0][1])
+# print the second column of the array
+print(array[:, 1])
+
+# create a 3D array with numpy library
+array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+# print the array with all the elements
+print(array)
+
+# create a matrix with numpy library and print it
+matrix = np.matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(matrix)
+
+# print the size of the matrix and the array
+print(matrix.size)
+print(array.size)
+
+# print the memory size of the matrix and the array
+print(matrix.nbytes)
+print(array.nbytes)
+
+# create a 3D array with numpy library and print it using full method
+array = np.full((3, 3), 5)
+print(array)
+array = np.zeros([3, 3])
+print(array)
---
+++ games_python/authentication.py
@@ -0,0 +1,70 @@
+import time
+import os
+
+WAIT_TIME_FILE = "txt/wait_time.txt"
+
+def ask_password(attempts=3):
+    wait_time = get_wait_time()
+    
+    if attempts == 0:
+        print("Maximum number of attempts exceeded. Please try again after {} minutes.".format(wait_time // 60))
+        update_wait_time(wait_time * 2)
+        time.sleep(wait_time)
+        return False
+    
+    password = input("Enter your password: ")
+    
+    try:
+        password = int(password)
+        if password == 1234:
+            print("Password is correct")
+            update_wait_time(wait_time = 60)
+            return True
+        else:
+            print("Password is incorrect")
+            return ask_password(attempts-1)
+
+    except ValueError:
+        print("Password is incorrect")
+        return ask_password(attempts-1)
+
+def get_wait_time():
+    if os.path.exists(WAIT_TIME_FILE):
+        with open(WAIT_TIME_FILE, "r") as file:
+            wait_time = int(file.read())
+    else:
+        wait_time = 60  # Default wait time of 1 minute
+    
+    return wait_time
+        
+def update_wait_time(wait_time):
+    # create a subdirectory if it doesn't exist called txt
+    if not os.path.exists("txt"):
+        os.makedirs("txt")
+    # create in the directory txt a file called wait_time.txt and write the wait_time in it
+    with open(WAIT_TIME_FILE, "w") as file: 
+        file.write(str(wait_time))
+        
+ask_password()
+
+# do the same thing using os.path.dirname
+# import os
+# WAIT_TIME_FILE = "txt/wait_time.txt"
+#
+# def get_wait_time():
+#     if os.path.exists(WAIT_TIME_FILE):
+#         with open(WAIT_TIME_FILE, "r") as file:
+#            wait_time = int(file.read())
+#     else:
+#        wait_time = 60  # Default wait time of 1 minute
+#
+#     return wait_time
+# 
+# def update_wait_time(wait_time):
+#     os.makedirs(os.path.dirname(WAIT_TIME_FILE), exist_ok=True)  # Create the "txt" subfolder if it doesn't exist
+#     with open(WAIT_TIME_FILE, "w") as file:
+#         file.write(str(wait_time))
+#
+# ask_password()
+
+# Path: games_python/authentication.py
